da6385078b38c390d272331e0dfdf9a8498aabe3
James Risberg <jimmyrisberg56@gmail.com>
2016-01-15 20:46:21 -0500

    Add submission form and CSS

--- listingController.js
+++ listingController.js
@@ -3,18 +3,21 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
     $scope.showEdit = false;
+    $scope.newListing = {}
 
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
     $scope.addListing = function() {
-      $scope.listings.push($scope.newItem);
-      $scope.newItem = '';
+      $scope.listings.push($scope.newListing);
+      $scope.newListing = {};
     };
+      
     $scope.deleteListing = function(index) {
       $scope.listings.splice(index, 1);
     };
+      
     $scope.showDetails = function(index) {
       $scope.name = $scope.listings[index].name;
       $scope.code = $scope.listings[index].code;
@@ -22,6 +25,7 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       $scope.longitude = $scope.listings[index].coordinates.longitude;
       $scope.address = $scope.listings[index].address;
     };
+      
     $scope.toggleEdit = function() {
       if ($scope.showEdit) {
         $scope.showEdit = false;
